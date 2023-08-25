4f80b1f238febf75473e3515f22d783d8bda5268
dghnts <mittsulann73@gmail.com>
2023-08-25 10:30:42 +0900

    getメソッドの修正

--- message/views.py
+++ message/views.py
@@ -3,9 +3,14 @@ from django.views import View
 
 from .models import Message
 from .form import MessageForm
+
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "index.html")
+        
+        messages = Message.objects.all()
+        context = {"messages":messages}
+        
+        return render(request, "index.html",context)
 
     def post(self, request, *args, **kwargs):
         form = MessageForm(request.POST)
@@ -15,6 +20,7 @@ class IndexView(View):
             errors = form.errors
             print(errors)
             
+            #errorsを変数としてtemolateで受け取るためにrenderを使用した
             return render(request, 'index.html', {"error": errors})
         
         print("バリデーションOK")
